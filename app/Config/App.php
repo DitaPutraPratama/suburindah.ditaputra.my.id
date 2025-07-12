1402b0ab45b0c4be1da4cfb7f68fdafdccf7b2b7
DitaPutraPratama <ditaputra1296@gmail.com>
2025-07-13 04:30:38 +0700

    update base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,9 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'https://suburindah.ditaputra.my.id/';
-    public string $baseURL = 'http://localhost:8080/';
+    // public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = getenv('app.baseURL') ?: 'http://localhost:8080/';
+
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
